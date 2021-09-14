092e16ef83c87bf309d8e982712cb249f128f3a7
kevin-funderburg <ksfunderburg@icloud.com>
2021-09-14 13:36:24 -0500

    added paintShape()

--- s1/main.cpp
+++ s1/main.cpp
@@ -47,23 +47,110 @@ void myDisplay(void)
 }
 
 
+//void paint(char shape)
+//{
+//	switch
+//}
+
+void paintPoint()
+{
+	glClear(GL_COLOR_BUFFER_BIT);     // clear the screen
+	
+	// Dotted line
+	glPushMatrix();       // Save model-view matrix setting
+	glBegin(GL_POINTS);
+		 glColor3f ( 2.0, 0.5, 0.0);
+		 glVertex3f(-0.0, 4.0, 0.0);
+		 glVertex3f( 1.0, 4.0, 0.0);
+		 glVertex3f( 2.0, 4.0, 0.0);
+		 glVertex3f( 3.0, 4.0, 0.0);
+		 glVertex3f( 4.0, 4.0, 0.0);
+	glEnd();
+	glPopMatrix();			// Restore the model-view matrix
+
+	glFlush();          	// send all output to display
+}
+
+
+void paintShape(char shape)
+{
+	float radius = 0.5;
+	
+	glClear(GL_COLOR_BUFFER_BIT);     //clear the screen
+	glPushMatrix();                   //save model-view matrix setting
+	
+	switch(shape)
+	{
+		case 'c':	//circle
+			break;
+			
+		case 'h':	//hexagon
+			break;
+			
+		case 'l':	//white line
+			glTranslatef(-2.5, 1.5, 0);
+			glRotatef(-90, 0, 1, 1);
+			glBegin(GL_LINES);
+				glColor3f ( 1.0, 1.8, 4.0);
+				glVertex2f(-3.5, 3.5);
+				glColor3f(0.2f, 0.2f, 0.2f); // Dark Gray
+				glVertex2f( 3.5, 3.5);
+			glEnd();
+			
+			break;
+			
+		case 'p':	//point
+			glBegin(GL_POINTS);
+				 glColor3f ( 2.0, 0.5, 0.0);
+				 glVertex3f(-0.0, 4.0, 0.0);
+				 glVertex3f( 1.0, 4.0, 0.0);
+				 glVertex3f( 2.0, 4.0, 0.0);
+				 glVertex3f( 3.0, 4.0, 0.0);
+				 glVertex3f( 4.0, 4.0, 0.0);
+			glEnd();
+			break;
+			
+		case 's':	//sphere
+			break;
+			
+		case 't':	//triangle
+			break;
+			
+		case 'u':	//cube
+			break;
+			
+		default:
+			break; 	//do nothing
+	}
+	
+	glPopMatrix();       // Restore the model-view matrix
+	glFlush();          	// send all output to display
+}
+	
 void myKeyboard(unsigned char theKey, int mouseX, int mouseY)
 {
 	switch(theKey)
 	{
 		case 'c':
+			paintShape('c');
 			break;
 		case 'h':
+			paintShape('h');
 			break;
 		case 'l':
+			paintShape('l');
 			break;
 		case 'p':
+			paintShape('p');
 			break;
 		case 's':
+			paintShape('s');
 			break;
 		case 't':
+			paintShape('t');
 			break;
 		case 'u':
+			paintShape('u');
 			break;
 		case 'Q':
 		case 'q':
@@ -86,3 +173,4 @@ int main(int argc, char** argv)
 	myInit();
 	glutMainLoop(); 		     // go into a perpetual loop
 }
+	
